ec2d94163b9a7f1465af69abb8774a0e021a8b39
0x0-14 <saigklkrishnan@gmail.com>
2023-12-05 15:52:35 +0530

    Remove unused BaseCubit

--- lib/utils/base_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-abstract class BaseCubit<T> extends Cubit<T> {
-  BaseCubit(super.state);
-
-  /// Initialize the cubit
-  Future<void> init();
-}
